c6719608d7e26fcbc1a8952abd94ffeb45fa8c10
nafkhanzam <nafkhanalzamzami@gmail.com>
2021-02-10 23:01:00 +0700

    remove passwordUtils

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import {errorUtils, JWTUtils} from "@nafkhanzam/backend-utils";
-import {ErrorStatus, validatorUtils, zod} from "@nafkhanzam/common-utils";
+import {ErrorStatus} from "@nafkhanzam/common-utils";
 import {AccessTokenJWT, accessTokenJWTValidator, Role} from "./context";
 import {env} from "./env";
 
@@ -7,18 +7,6 @@ export const jwtUtils = new JWTUtils(
   accessTokenJWTValidator,
   env.JWT_LOGIN_KEY,
 );
-
-export const passwordUtils = (password: string): boolean => {
-  if (password.includes(" ")) {
-    return false;
-  }
-  try {
-    validatorUtils.validate(zod.string().min(6), password);
-  } catch (error) {
-    return false;
-  }
-  return true;
-};
 
 export const validJwt = (
   jwt: AccessTokenJWT | null,
